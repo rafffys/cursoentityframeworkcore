a33ce350d9b9c646ba8bd4505af9a9c5648eb1a8
Rafa Rodrigues <rafffys@gmail.com>
2019-03-01 23:09:41 -0300

    Primeira entidade

---
+++ Switch.Domain/Entities/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Switch.Domain.Enums;
+
+namespace Switch.Domain.Entities
+{
+    public class Usuario
+    {
+        public int Id { get; private set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string UrlFoto { get; set; }
+        public SexoEnum Sexo { get; set; }
+    }
+}
---
+++ Switch.Domain/Enums/SexoEnum.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Switch.Domain.Enums
+{
+    public enum SexoEnum
+    {
+        NaoDefinido = 1,
+        Masculino = 2,
+        Feminino = 3
+    }
+}
